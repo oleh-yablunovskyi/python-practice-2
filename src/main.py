b595e6f3b15a33d63a91d81df4f1cb35275d563f
Oleh Yablunovskyi <lifserv@gmail.com>
2025-09-26 09:09:01 +0300

    feat: update main to convert CSV to JSON using CsvToJsonConverter

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-from CsvReader import CsvReader
+from CsvToJsonConverter import CsvToJsonConverter
 
 if __name__ == "__main__":
     csv_url = "https://informer.com.ua/dut/python/import/st_gt.csv"
-    csv_reader = CsvReader()
-    data = csv_reader.read_data(csv_url)
-    for row in data:
-        print(row)
+    json_path = "students_data.json"  # Шлях до файлу, куди буде збережено JSON
+
+    converter = CsvToJsonConverter()
+    converter.read_and_convert(csv_url, json_path)
