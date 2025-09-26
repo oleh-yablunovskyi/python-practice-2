145c1efe6fe4aaf07c23289c301de9b9e1c13bdf
Oleh Yablunovskyi <lifserv@gmail.com>
2025-09-26 09:12:06 +0300

    feat: update main to display JSON data using JsonReader

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
+from JsonReader import JsonReader
 from CsvToJsonConverter import CsvToJsonConverter
 
 if __name__ == "__main__":
     csv_url = "https://informer.com.ua/dut/python/import/st_gt.csv"
-    json_path = "students_data.json"  # Шлях до файлу, куди буде збережено JSON
+    json_path = "students_data.json"  # Шлях до файлу JSON
 
+    # Ініціалізація CsvToJsonConverter та виконання конвертації
     converter = CsvToJsonConverter()
     converter.read_and_convert(csv_url, json_path)
+
+    # Читання та відображення даних з JSON
+    json_reader = JsonReader()
+    json_reader.display_data(json_path)
